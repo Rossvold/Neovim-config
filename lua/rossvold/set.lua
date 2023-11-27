55cdf325ec81f18ef9acd8184940a1a2415ed10a
Rossvold <rubenrossvold@icloud.com>
2023-11-27 20:07:48 +0100

    more for tailwind scrubs

--- lua/rossvold/set.lua
+++ lua/rossvold/set.lua
@@ -3,6 +3,9 @@ vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50"
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+-- this is because i'm a tailwindcss scrub
+vim.opt.conceallevel = 2
+
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
@@ -28,7 +31,6 @@ vim.opt.updatetime = 50
 
 -- Set a column at 120 characters to keep lines from getting too long
 vim.opt.colorcolumn = "80"
-vim.opt.wrap = true
-
+vim.opt.wrap = false
 
 vim.g.mapleader = " "
